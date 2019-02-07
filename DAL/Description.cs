3604610c531171cbdf115b55eba7b8a4c2a38628
andriy1234 <andriy1234@A34COMP7>
2019-02-07 17:44:39 +0200

    Add project files.

---
+++ DAL/Description.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Description
+    {
+        public int ID { get; set; }
+        //[ForeignKey("Product")]
+        public int ID_Product { get; set; }
+        public string Descriptions { get; set; }
+    }
+}
---
+++ Site1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+namespace Site1.Controllers
+{
+    public class HomeController : Controller
+    {
+        DAL.DBase ctx = new DAL.DBase();
+        public ActionResult Index()
+        {
+
+          
+            return View(ctx.Products);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GoLink(string id)
+        {
+            return Redirect(ctx.Products.FirstOrDefault(x => x.Name == id).URL);
+        }
+
+    }
+}
